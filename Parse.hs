8ae046ed65358a12c253c39093afa1452b853ca4
Joel Williamson <joel.s.williamson@gmail.com>
2015-04-02 15:39:50 -0400

    Move the AST data type into its own module

--- Parse.hs
+++ Parse.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 module Parse where
 
 import qualified Lexer as L
+import AST
+
 import Text.Parsec
 import Data.Text hiding (break)
 
@@ -9,36 +12,6 @@ import Control.Applicative hiding ((<|>),many)
 
 import Prelude hiding (break,sequence)
 
-type Identifier = Text
-
-data CompareOp = Eq | Neq | Gt | Lt | Gte | Lte
-               deriving (Show,Eq)
-
-data Expr = LValueId Identifier
-          | LValueField Expr Identifier
-          | LValueSubscript Expr Expr
-          | Nil
-          | Seq [Expr] -- Has value of the last expr
-          | Void -- () or let ... in end;
-          | IntLit Integer
-          | StringLit Text
-          | Negation Expr
-          | FunctionCall Identifier [Expr] -- This has a value if it is a function, none for procedure
-          | Add Expr Expr | Sub Expr Expr
-          | Mult Expr Expr | Div Expr Expr
-          | Comp CompareOp Expr Expr
-          | And Expr Expr
-          | Or Expr Expr
-          | Record Identifier [(Identifier,Expr)] -- typeid {id=exp,id=exp,...}
-          | Array Identifier Expr Expr -- typeid [n] of v, duplicates v n times
-          | Assignment Expr Expr -- Compounds share backing and are never released
-          | IfThenElse Expr Expr Expr -- The second two exprs must have the same type
-          | IfThen Expr Expr -- The second expr must not produce a value
-          | While Expr Expr -- e2 produces no value
-          | For Identifier Expr Expr Expr -- for id := e1 to e2 do e3
-          | Break
-          | Let [Decl] Expr -- let decs in exps end
-            deriving (Show,Eq)
 
 parseTiger = parse (L.ws *> expression) ""
 
@@ -105,18 +78,6 @@ for = For <$> (L.for' *> identifier) <*> (L.assign' *> expression) <*> (L.to' *>
 break = Break <$ L.break'
 letE = Let <$> (L.let' *> many decl <* L.in') <*> (expression <* L.end')
 
-
-data Decl = TypeDec Identifier Type
-          | VarDec Identifier Expr
-          | TVarDec Identifier Identifier Expr -- id : tpye-id := expr
-          | FunDec Identifier [(Identifier,Identifier)] Expr
-          | TFunDec Identifier [(Identifier,Identifier)] Identifier Expr -- function id (a1:t1,a2:t2,...) :tr = expr
-          deriving (Show,Eq)
-
-data Type = NamedType Identifier
-          | RecType [(Identifier,Identifier)] -- { f1:t1,f2:t2 ...}
-          | ArrType Identifier
-          deriving (Show,Eq)
 
 parseD = parse decl ""
 
